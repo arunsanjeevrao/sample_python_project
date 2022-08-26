f155968e6c8e9a754fe22bf538fa2086b8bccf38
Arun Sanjeeva <sanjeevaarun@airtory.com>
2022-08-26 11:41:57 +0530

    Added New Features

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import os
 
 print("Starting the program...\n")
 print("The items in the root directory: ", os.listdir())
-df = pd.read_csv('6f104bbb5dc80fcbbac2cce23ca18d76.csv')
+df = pd.read_csv('9cfb1200eb45d829f4af96480a118238.csv')
 print("The first 5 records in the table: \n", df.head())
+
+# New features
+print("The Number of Records in the table: ", len(df))
+print("\nSummary Statistics: \n\n", df.describe())
